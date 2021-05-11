664e35e06279ecdd6e56c94010fdcebb4d896745
SheilaTran-UCF <ngochue2076@gmai.com>
2021-05-11 15:10:09 +0700

    add comment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,8 @@ import {combineReducers} from 'redux';
 import authReducer from './authReducer';
 import movieReducer from './movieReducer';
 
+// add comment
+
 export default combineReducers({
   auth: authReducer,
   movies: movieReducer,
